589ce2b0a7dac9fd1c6fd8785a4d761ae029d68a
HannahMcKay1 <hromaniuk@yahoo.ca>
2026-01-18 20:17:47 -0700

    I added the pokemon api service and fixed the http error

--- assignment1-HannahMcKay1-main/lib/services/pokemon_api.dart
+++ assignment1-HannahMcKay1-main/lib/services/pokemon_api.dart
@@ -1,6 +1,6 @@
 //Importing http so we can call the web tools and importing dart:convert to help me turn long strings of text into data.
-import 'package:http/http.dart' as http; 
-import 'dart:convert'; 
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 //address for pokemon API
 const String pokemonEndpoint = 'https://pokeapi.co/api/v2/pokemon';
@@ -8,7 +8,6 @@ const String pokemonEndpoint = 'https://pokeapi.co/api/v2/pokemon';
 // function to find a specific Pokemons data
 Future<dynamic> getPokemonByName({required String pokemonName}) async {
   try {
-    
     //I combined the address with the pokemon name to get the full website link
     final url = Uri.parse('$pokemonEndpoint/$pokemonName');
     //awaiting a response
@@ -16,18 +15,22 @@ Future<dynamic> getPokemonByName({required String pokemonName}) async {
     //checking the status. 200 is good
     if (response.statusCode != 200) {
       //if the website is down or the name is spelled incorrectly, there needs to be an explanation
-      throw Exception('There was a problem with the request: status ${response.statusCode} received');
+      throw Exception(
+        'There was a problem with the request: status ${response.statusCode} received',
+      );
     }
 
     //taking text from the website and turning it into a map
     return jsonDecode(response.body);
-    
   } catch (e) {
-    //If theres an connection issue
+    // Exception
     if (e.toString().contains('status')) {
-      rethrow; 
+      throw Exception('The server returned a status error: ${e.toString()}');
     }
-    //any other error messages 
-    throw Exception('There was a problem with the request: ${e.toString().replaceFirst('Exception: ', '')}');
+
+    // For any other unexpected errors
+    throw Exception(
+      'There was a problem with the request: ${e.toString().replaceFirst('Exception: ', '')}',
+    );
   }
 }
